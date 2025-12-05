aa9bc0d9c0027b928ef16f7fc1e3d80970bc3928
Alok mishra <alokmishra9861@gmail.com>
2025-12-05 17:46:01 +0530

    initial commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,45 +1,45 @@
- const User=require("../models/user");
+const User = require("../models/user");
 
-module.exports.renderSignupform=(req,res)=>{
-    res.render("users/signup.ejs");
- }
+module.exports.renderSignupform = (req, res) => {
+  res.render("users/signup.ejs");
+};
 
- module.exports.signup=async(req,res)=>{
-       try{
-    let{username,email,password}=req.body;
+module.exports.signup = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
     // console.log(req.body);
-    const newUser= new User({email , username});
-    const registeredUser=await User.register(newUser,password);
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
     console.log(registeredUser);
-    req.login(registeredUser,(err)=>{
-     if(err){
-       return next(err);
-     }
-     req.flash("success","welcome to Wanderlust");
-     res.redirect("/listings");
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "welcome to Wanderlust");
+      res.redirect("/listings");
     });
-       }catch(e){
-          req.flash("error",e.message)
-          res.redirect("/signup");
-       }
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
   }
+};
 
-  module.exports.renderLoginform=(req,res)=>{
-   res.render("users/login.ejs")
- }
+module.exports.renderLoginform = (req, res) => {
+  res.render("users/login.ejs");
+};
 
- module.exports.login= async(req,res)=>{
-   req.flash("success","welcome back to wanderlust");
-   let redirectUrl=res.locals.redirectUrl || "/listings";//if there is any data in res.locals.redirectedUrl then go to that if not go to /listings
-   res.redirect(redirectUrl);
- }
+module.exports.login = async (req, res) => {
+  req.flash("success", "welcome back to wanderlust");
+  let redirectUrl = res.locals.redirectUrl || "/listings"; //if there is any data in res.locals.redirectedUrl then go to that if not go to /listings
+  res.redirect(redirectUrl);
+};
 
- module.exports.logout=(req,res,next)=>{
-  req.logout((err)=>{
-    if(err){
+module.exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
       return next(err);
     }
-    req.flash("success","you are logged out");
+    req.flash("success", "you are logged out");
     res.redirect("/listings");
   });
- }
+};
